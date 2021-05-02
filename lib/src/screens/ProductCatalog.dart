dfa5b5c6d1cf22bf2d86890462a151e51a52f24f
MadhavShah <madhavshahsisodiya@gmail.com>
2021-05-02 12:14:21 +0530

    ll

--- lib/src/screens/ProductCatalog.dart
+++ lib/src/screens/ProductCatalog.dart
@@ -9,7 +9,7 @@ class ProductCatalog extends StatefulWidget {
 }
 
 class _ProductCatalogState extends State<ProductCatalog> {
-  List<Posts> postList = [];
+  List<Posts> postList;
   String uid;
 
   Future<List<Posts>> getPosts() async {
@@ -19,44 +19,47 @@ class _ProductCatalogState extends State<ProductCatalog> {
         FirebaseFirestore.instance.collection('Customer_Details');
     SharedPreferences prefs = await SharedPreferences.getInstance();
     uid = prefs.getString("uid");
-    DocumentSnapshot userSnapshot = await users.doc(uid).get();
-    print(userSnapshot.data()['Following']);
+    users.doc(uid).get().then((DocumentSnapshot userSnapshot) {
+      print(userSnapshot.data()['Following']);
 
-    userSnapshot.data()['Following'].forEach((brandId) async {
-      CollectionReference brands =
-          FirebaseFirestore.instance.collection('Brand_Details');
-      DocumentSnapshot snapshot = await brands.doc(brandId).get();
-      print(brandId);
-      print(snapshot.data());
-      // postIds = snapshot.data()['posts'];
+      userSnapshot.data()['Following'].forEach((brandId) async {
+        CollectionReference brands =
+            FirebaseFirestore.instance.collection('Brand_Details');
+        DocumentSnapshot snapshot = await brands.doc(brandId).get();
+        print(brandId);
+        print(snapshot.data());
+        // postIds = snapshot.data()['posts'];
 
-      snapshot.data()['posts'].forEach((postId) async {
-        CollectionReference posts =
-            FirebaseFirestore.instance.collection('Post_Details');
-        DocumentSnapshot postSnapshot = await posts.doc(postId).get();
-        String productId = postSnapshot.data()['Product_Id'];
-        CollectionReference products =
-            FirebaseFirestore.instance.collection('Product_Details');
-        DocumentSnapshot productSnapshot = await products.doc(productId).get();
-        // print(productSnapshot.data());
+        snapshot.data()['posts'].forEach((postId) async {
+          CollectionReference posts =
+              FirebaseFirestore.instance.collection('Post_Details');
+          DocumentSnapshot postSnapshot = await posts.doc(postId).get();
+          String productId = postSnapshot.data()['Product_Id'];
+          CollectionReference products =
+              FirebaseFirestore.instance.collection('Product_Details');
+          DocumentSnapshot productSnapshot =
+              await products.doc(productId).get();
+          print(productSnapshot.data());
 
-        Product product = new Product(
-            productSnapshot.data()['Product_Image'],
-            productSnapshot.data()['Product_Name'],
-            double.parse(productSnapshot.data()['Price']),
-            productSnapshot.data()['Brand_Id'],
-            int.parse(productSnapshot.data()['Quantity_In_Stock']),
-            productSnapshot.data()['Description']);
-        // print(product.product_Image);
-        Posts newPost = new Posts(
-            product,
-            productId,
-            postSnapshot.data()['No_of_Comments'],
-            postSnapshot.data()['No_of_Likes'],
-            postSnapshot.data()['Comments'].cast<String>(),
-            postSnapshot.data()['Likes'].cast<String>());
-        postList.add(newPost);
-        print(postList.length);
+          Product product = new Product(
+              productSnapshot.data()['Product_Image'],
+              productSnapshot.data()['Product_Name'],
+              double.parse(productSnapshot.data()['Price']),
+              productSnapshot.data()['Brand_Id'],
+              int.parse(productSnapshot.data()['Quantity_In_Stock']),
+              productSnapshot.data()['Description']);
+          print(product);
+          Posts newPost = new Posts(
+              product,
+              productId,
+              postSnapshot.data()['No_of_Comments'],
+              postSnapshot.data()['No_of_Likes'],
+              postSnapshot.data()['Comments'].cast<String>(),
+              postSnapshot.data()['Likes'].cast<String>());
+          postList.add(newPost);
+          print(postList.length);
+          return postList;
+        });
       });
     });
     print(postList.length);
@@ -69,15 +72,12 @@ class _ProductCatalogState extends State<ProductCatalog> {
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting)
           return CircularProgressIndicator();
-        else if (snapshot.connectionState == ConnectionState.done)
-          {
-            print(snapshot.data.toString());
-            return Center(
-              child: Text('DOne'),
-            );
-          }
-
-        else
+        else if (snapshot.connectionState == ConnectionState.done) {
+          print(snapshot.data.toString());
+          return Center(
+            child: Text('DOne'),
+          );
+        } else
           return Center(
             child: Text('Error Occured'),
           );
